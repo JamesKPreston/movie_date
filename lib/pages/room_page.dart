e59ec467f10b4816acdba84e600f7a88eecb078e
James <jamesp@spindance.com>
2025-01-11 15:40:51 -0500

    further match and filter update refinements

--- lib/pages/room_page.dart
+++ lib/pages/room_page.dart
@@ -8,7 +8,7 @@ import 'package:movie_date/pages/actor_page.dart';
 import 'package:movie_date/pages/main_page.dart';
 import 'package:movie_date/pages/match_found_page.dart';
 import 'package:movie_date/tmdb/providers/genre_repository_provider.dart';
-import 'package:movie_date/supabase/providers/movie_choices_channel_provider.dart';
+import 'package:movie_date/providers/movie_choices_channel_provider.dart';
 import 'package:movie_date/providers/profile_repository_provider.dart';
 import 'package:movie_date/providers/room_service_provider.dart';
 import 'package:movie_date/widgets/calendar_widget.dart';
